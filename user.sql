088c3fffee632b0f01310b3f463c5e50d995da4b
HoangFuc <130912005+HoangFuc@users.noreply.github.com>
2023-08-30 10:11:30 +0700

    Add files via upload

---
+++ user.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE school;
+CREATE TABLE inforuser (
+	id int PRIMARY KEY,
+	mssv varchar(50),
+	password varchar(50),
+	class varchar(10),
+	fullName varchar(10),
+	birthday DATE,
+	major varchar(10),
+	gender varchar(10)
+	);
